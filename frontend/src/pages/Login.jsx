5ac9d09429a6159289fa1210711cbe18fbf4b8d2
Jahangir-Hossain99 <smithjack0153@gmail.com>
2026-08-17 14:00:01 +0600

    Login URL has been updated

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useAuth } from "../context/AuthContext";
 import toast from "react-hot-toast";
 import axios from "axios";
+import import.meta.env.VITE_API_BASE_URL || "http://localhost:5000"; // Fallback to local if env variable is not set
 
 
 
@@ -13,7 +14,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try{
-      const response = await axios.post(`http://localhost:5000/login`, { email, password,role:"user" });
+      const response = await axios.post(`${import.meta.env.VITE_API_BASE_URL}login`, { email, password,role:"user" });
       const token = response.data.token
       const user = response.data.user;
       await login(user,token)
